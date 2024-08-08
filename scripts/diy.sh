7ff4ea08ccb22edeff858fe82ffb46d8b8884318
shuishihan <11583895+shuishihan@users.noreply.github.com>
2024-08-08 22:01:27 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,4 +2,5 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
+#删除冲突文件
+rm -rf $(find /workdir/openwrt/package -type d -regex ".*\(istore\|example\).*")
